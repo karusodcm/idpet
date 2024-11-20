89672bf82f90a4a4bc14b31fa0c4cedbc355a9c3
karlsdicks <karlsdcm@gmail.com>
2024-11-19 19:03:34 -0500

    login

--- js/index.js
+++ js/index.js
@@ -1,32 +1,32 @@
-const red = document.getElementById('red')
-const green = document.getElementById('green')
-const circle = document.getElementById('circle')
-const rombo = document.getElementById('rombo')
-const shadow = document.querySelector('.i-shadown-placa')
-const cc = document.querySelector('.i-example-placaqr')
-const placa = document.getElementById('placa')
+// const red = document.getElementById('red')
+// const green = document.getElementById('green')
+// const circle = document.getElementById('circle')
+// const rombo = document.getElementById('rombo')
+// const shadow = document.querySelector('.i-shadown-placa')
+// const cc = document.querySelector('.i-example-placaqr')
+// const placa = document.getElementById('placa')
 
-red.addEventListener('click',()=>{
-    placa.style.border = '10px solid red'
-})
+// red.addEventListener('click',()=>{
+//     placa.style.border = '10px solid red'
+// })
 
-green.addEventListener('click',()=>{
-    placa.style.border = '10px solid green'
-})
+// green.addEventListener('click',()=>{
+//     placa.style.border = '10px solid green'
+// })
 
-circle.addEventListener('click',()=>{
-    placa.style.width = '190px'
-    placa.style.height = '190px'
-    shadow.style.display = 'none'
-    placa.style.borderRadius = '50%'
-    cc.style.marginTop = '-1rem'
-})
+// circle.addEventListener('click',()=>{
+//     placa.style.width = '190px'
+//     placa.style.height = '190px'
+//     shadow.style.display = 'none'
+//     placa.style.borderRadius = '50%'
+//     cc.style.marginTop = '-1rem'
+// })
 
-rombo.addEventListener('click',()=>{
-    placa.style.width = '150px'
-    placa.style.height = '150px'
-    shadow.style.display = 'block'
-    placa.style.borderRadius = '.5rem'
-    cc.style.marginTop = '0'
-})
+// rombo.addEventListener('click',()=>{
+//     placa.style.width = '150px'
+//     placa.style.height = '150px'
+//     shadow.style.display = 'block'
+//     placa.style.borderRadius = '.5rem'
+//     cc.style.marginTop = '0'
+// })
 
---
+++ js/login.js
@@ -0,0 +1,3 @@
+const loginaccess = document.getElementById("loginaccess")
+const recoverypass = document.getElementById("recoverypass")
+const masterpet = document.getElementById("masterpet")
